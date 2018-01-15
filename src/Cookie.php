7acf319922aaa295830a5721805a5a52b552fa21
Greg Bowler <greg.bowler@g105b.com>
2018-01-15 23:27:52 +0000

    Basic functionality to satisfy test

--- src/Cookie.php
+++ src/Cookie.php
@@ -2,7 +2,25 @@
 namespace Gt\Cookie;
 
 class Cookie {
-	public function __construct(array $cookie) {
+	protected $name;
+	protected $value;
 
+	public function __construct(string $name, string $value = "") {
+		$this->name = $name;
+		$this->value = $value;
+	}
+
+	public function getName():string {
+		return $this->name;
+	}
+
+	public function getValue():string {
+		return $this->value;
+	}
+
+	public function withValue(string $value):self {
+		$clone = clone($this);
+		$clone->value = $value;
+		return $clone;
 	}
 }
