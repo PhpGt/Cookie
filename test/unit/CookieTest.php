b1b27c726edb594d40c04262841d137d9a1cc108
Greg Bowler <greg.bowler@g105b.com>
2018-01-15 23:27:31 +0000

    Initial getName test

---
+++ test/unit/CookieTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace Gt\Cookie\Test;
+
+use Gt\Cookie\Cookie;
+use Gt\Cookie\Validity;
+use PHPUnit\Framework\TestCase;
+
+class CookieTest extends TestCase {
+	/**
+	 * @dataProvider dataNameValue
+	 */
+	public function testGetName(string $name, string $value) {
+		$cookie = new Cookie($name, $value);
+		self::assertEquals($name, $cookie->getName());
+	}
+
+	public static function dataNameValue():array {
+		$data = [];
+
+		for($i = 0; $i < 10; $i++) {
+			$row = [
+				self::getRandomText(Validity::getValidNameCharacters()),
+				self::getRandomText(Validity::getValidValueCharacters()),
+			];
+			$data []= $row;
+		}
+
+		return $data;
+	}
+
+	protected static function getRandomText(
+		array $charSet,
+		int $minLength = 1,
+		int $maxLength = 100
+	):string {
+		$text = "";
+		$length = rand($minLength, $maxLength);
+
+		while(strlen($text) < $length) {
+			$randIndex = array_rand($charSet);
+			$text .= $charSet[$randIndex];
+		}
+
+		return $text;
+	}
+}
